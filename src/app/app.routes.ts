91773f2dd75bbae588c32bfac33ee31227024637
Varun <varunrajore7272@gmail.com>
2026-06-06 01:31:22 +0530

    some minot changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -47,11 +47,11 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/dashboard',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: '/dashboard'
+    redirectTo: 'dashboard'
   }
 ];
